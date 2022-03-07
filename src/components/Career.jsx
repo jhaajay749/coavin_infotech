01308329e1f081cf877ed25b534bc50ac01954fa
Ajay Kumar Jha <72486960+jhaajay749@users.noreply.github.com>
2022-03-07 20:39:30 +0530

    commit

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Career() {
   return (
-    <div className="row bg-dark m-5 p-5 text-light" id="section2">
+    <div className="row bg-dark m-1 p-5 text-light" id="section2">
       <h1 className="display-5 text-center">Career</h1>
       <p className="text-center">
         Send us your Resume or CV on our Business Email

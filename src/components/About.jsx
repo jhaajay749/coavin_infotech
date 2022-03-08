a60bdac4a2df19bc4f50f1ad4f5f84c0763d2982
Ajay Kumar Jha <72486960+jhaajay749@users.noreply.github.com>
2022-03-08 09:19:49 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <h1>About</h1>
       <h2>Looking for USA or UK Remote Job</h2>
       <p>
-        We have Team of Developers accros the India. we and my team take
+        We have Team of Developers across the India. we and my team take
         projects and clients from UK and USA. We hire developers on contract
         basis for USA and UK companies. Salary 1 Lakh to 3 Lakh per months in
         hand depends upon your skill sets. Job type  Remote( Work from Home )
